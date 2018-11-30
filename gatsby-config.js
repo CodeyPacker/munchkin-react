04415c9b5056debe58c930b7eaf5f035132a3173
CodeyPacker <codeypacker@gmail.com>
2018-11-29 23:38:37 -0700

    Played around with some styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Munchkin Battle Tracker',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,14 @@ const Header = ({ siteTitle }) => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      marginBottom: '.75rem',
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '.5rem 1.0875rem',
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -21,6 +21,7 @@ const Header = ({ siteTitle }) => (
           style={{
             color: 'white',
             textDecoration: 'none',
+            fontSize: '25px',
           }}
         >
           {siteTitle}
