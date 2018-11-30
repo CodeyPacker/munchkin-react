cfe9fde9b5092b659bdfa2d37b1aa708a723b1d0
CodeyPacker <codeypacker@gmail.com>
2018-11-30 00:28:41 -0700

    Added a basic MonsterCard component

---
+++ src/components/MonsterCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MonsterCard = () => (
+  <div style={{ 
+    maxWidth: '300px',
+    margin: '0 auto', 
+    marginBottom: '1.45rem', 
+    border:'1px solid black', 
+    padding: '15px', 
+  }}>
+
+    <h3>Monster Name</h3>
+    <p>Monster Level</p>
+    <p>Monster Abilities</p>
+  </div>
+)
+
+export default MonsterCard
