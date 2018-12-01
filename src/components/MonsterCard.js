55953148b79566b332b31f39c816cd859bdcc697
CodeyPacker <codeypacker@gmail.com>
2018-12-01 13:26:50 -0700

    Print out all monster cards to the DOM

--- src/components/MonsterCard.js
+++ src/components/MonsterCard.js
@@ -1,18 +1,26 @@
 import React from 'react'
+const styles = {
+  width: '300px',
+  display: 'inline-block',
+  minHeight: '375px',
+  margin: '20px',
+  padding: '15px',
+  borderRadius: '10px',
+  boxShadow: '0px 2px 10px 0px rgba(92,92,92,1)'
+}
 
-const MonsterCard = () => (
-  <div style={{ 
-    maxWidth: '300px',
-    margin: '0 auto', 
-    marginBottom: '1.45rem', 
-    border:'1px solid black', 
-    padding: '15px', 
-  }}>
-
-    <h3>Monster Name</h3>
-    <p>Monster Level</p>
-    <p>Monster Abilities</p>
+function MonsterCard(props) {
+  return(
+    <div style={styles}>
+      <h3>{props.name}</h3>
+      <p>{`${props.set} set`}</p>
+      <p>{`Power: ${props.power}`}</p>
+      <p>{`Treasures: ${props.treasure}`}</p>
+      <p>{`Levels: ${props.levels}`}</p>
+      <p>{`Abilities: ${props.abilities}`}</p>
+      <p>{`Bad stuff: ${props.bad}`}</p>
   </div>
-)
+  )
+}
 
 export default MonsterCard
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import Layout from '../components/layout'
 import MonsterCard from '../components/MonsterCard'
+import monsterData from '../components/MonsterData'
 
-const IndexPage = () => (
-  <div>
-    <Layout />
-    <MonsterCard />
-  </div>
-)
+// const IndexPage = () => (
+//   <div>
+//   <Layout />
+//   <MonsterCard 
+//     info = {{
+//       name: "venom",
+//       set: "marvel",
+//       power: 10,
+//       treasure: 3,
+//       levels: 1,
+//       abilities: "-3 against Spider-friends. You may discard Klaw's Sonic Blaster to automatically defeat him (gain treasures, but no levels).",
+//       bad: "If you are wearing armor, you have -5 in your next combat. If not, -10!"
+//     }}
+//   />
+//   </div>
+// )
 
+function IndexPage() {
+  const monsterCards = monsterData.map(monster => 
+    <MonsterCard 
+      key={monster.name}
+      name={monster.name}
+      set={monster.set}
+      power={monster.power}
+      treasure={monster.treasure}
+      levels={monster.levels}
+      abilities={monster.abilities}
+      bad={monster.bad}
+    />)
+
+  return (
+    <div>
+        {monsterCards}            
+    </div>
+  )
+}
 export default IndexPage
